ebfe1a490e1d44ff4bf3ad813374f26b02e600cf
Chen <chen2468830035@gmail.com>
2021-05-08 20:47:51 +0200

    WindowsLaunch....

--- src/Client/Networking/WindowsLaunch.java
+++ src/Client/Networking/WindowsLaunch.java
@@ -1,12 +1,9 @@
 package Client.Networking;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class WindowsLaunch extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+public class WindowsLaunch {
+    public static void main(String[] args) {
         Application.launch(ClientStart.class);
     }
 }
